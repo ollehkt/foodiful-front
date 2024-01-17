3c5ca6603795a3991c46edc2d3c53b420db84626
davidktlee <rudxor567@naver.com>
2024-01-17 11:15:07 +0900

    feat: 모달 보여줄 때 주변 화면 어둡게 처리

--- components/common/modal/ModalContainer.tsx
+++ components/common/modal/ModalContainer.tsx
@@ -18,19 +18,20 @@ function ModalContainer() {
   return (
     <>
       {modal.isOpen && (
-        <div
-          className={`fixed top-1/3 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[300px] h-[200px] bg-[#fff] shadow-basic rounded-md z-[999999]`}
-        >
-          <div className="flex justify-center text-2xl text-red my-4">
-            <MdInfo />
+        <>
+          <div className="fixed inset-0 bg-black bg-opacity-50 z-[99999]"></div>
+          <div className="fixed top-1/3 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[300px] h-[200px] bg-[#fff] shadow-basic rounded-md z-[999999]">
+            <div className="flex justify-center text-2xl text-red my-4">
+              <MdInfo />
+            </div>
+            <div className="text-center text-main text-xl font-bold my-2">{modal.title}</div>
+            <div className="text-center font-semibold my-4">{modal.content}</div>
+            <div className="flex justify-center items-center gap-4 my-4">
+              <Button title="확인" onClick={onClickConfirm} style="bg-main text-white" size="sm" />
+              <Button title="취소" onClick={onClickCancel} style="" size="sm" />
+            </div>
           </div>
-          <div className="text-center text-main text-xl font-bold my-2">{modal.title}</div>
-          <div className="text-center font-semibold my-4">{modal.content}</div>
-          <div className="flex justify-center items-center gap-4 my-4">
-            <Button title="확인" onClick={onClickConfirm} style="bg-main text-white" size="sm" />
-            <Button title="취소" onClick={onClickCancel} style="" size="sm" />
-          </div>
-        </div>
+        </>
       )}
     </>
   )
