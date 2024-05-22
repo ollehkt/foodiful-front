f48fbfc0340cf1205dad9fec160357ea29dceef7
davidktlee <rudxor567@naver.com>
2024-05-22 15:45:22 +0900

    feat: 404, 500 에러 페이지 추가

---
+++ pages/500.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function Custom500() {
+  return (
+    <div className="w-[80%] mx-auto my-20 gap-y-4 flex flex-col justify-center items-center">
+      <p className="text-4xl text-main">500 Server Error</p>
+      <p className="text-4xl text-main">페이지를</p>
+      <p className="text-4xl text-main">찾을 수 없습니다.</p>
+      <Link
+        href="/"
+        className="text-3xl p-2 border-2 border-main rounded-lg hover:bg-main hover:text-white"
+      >
+        홈으로
+      </Link>
+    </div>
+  )
+}
