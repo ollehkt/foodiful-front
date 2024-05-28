2690261adf7ee8400a26ef88f7b674ad52dd4344
davidktlee <rudxor567@naver.com>
2024-05-28 21:01:19 +0900

    fix: 슬라이더 이미지 수정 및 소개 내용 수정

--- components/common/MainSlider.tsx
+++ components/common/MainSlider.tsx
@@ -54,7 +54,7 @@ const MainSlider = ({ imgs }: { imgs: StaticImageData[] }) => {
       <Image
         src={imgs[index]}
         alt="slider-image"
-        className="w-[80%] md:w-[70%] mx-auto rounded-md"
+        className="w-[80%] md:w-[70%] mx-auto rounded-md object-contain"
       />
       {!isMobile && (
         <>
@@ -100,5 +100,111 @@ const MainSlider = ({ imgs }: { imgs: StaticImageData[] }) => {
     </div>
   )
 }
-
 export default MainSlider
+
+/**
+ * const total = imgs.length - 1
+  const [hoverState, setHoverState] = useState({ left: false, right: false })
+  const isMobile = useAtomValue(isMobileDisplay)
+  const onMouseEnter = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      const { name } = e.currentTarget
+      setHoverState({ ...hoverState, [name]: true })
+    },
+    [hoverState]
+  )
+
+  const onMouseLeave = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      const { name } = e.currentTarget
+      setHoverState({ ...hoverState, [name]: false })
+    },
+    [hoverState]
+  )
+
+  const scrollPrev = () => {
+    if (index <= 0) setIndex(total)
+    else setIndex((currentIdx) => (currentIdx -= 1))
+  }
+  const scrollNext = () => {
+    if (index === total) setIndex((currentIdx) => (currentIdx = 0))
+    else setIndex((currentIdx) => (currentIdx += 1))
+  }
+
+  const [index, setIndex] = useState(0)
+  const timeoutRef = useRef<any>(null) // 타입 찾기
+
+  const resetTimeout = () => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current)
+  }
+
+  useEffect(() => {
+    resetTimeout()
+    timeoutRef.current = setTimeout(() => {
+      setIndex((prevIdx) => (prevIdx === total ? 0 : prevIdx + 1))
+    }, 2500)
+
+    return () => resetTimeout()
+  }, [index, total])
+
+  return (
+    <div
+      className={`flex md:grid  md:grid-cols-3 md:grid-rows-1 w-[90%] mx-auto border-white rounded-md h-[300px] md:h-[600px] relative`}
+    >
+      {isMobile ? (
+        <Image
+          src={imgs[index]}
+          alt="slider-image"
+          className="w-[80%] md:w-[70%] mx-auto rounded-md object-contain"
+        />
+      ) : (
+        imgs.map((img, idx) => (
+          <Image key={`${img}-${idx}`} src={imgs[index]} alt="image" className="" />
+        ))
+      )}
+
+      {!isMobile && (
+        <>
+          <button
+            name="left"
+            className={`flex justify-center items-center absolute top-[45%] left-[10%] ${
+              hoverState.left
+                ? 'bg-main scale-125 ease-in duration-120 transition-all'
+                : 'bg-white scale-100 ease-in duration-120 transition-all'
+            } w-[40px] h-[40px] border-[1px] border-transparent rounded-3xl`}
+            onClick={scrollPrev}
+            onMouseEnter={(e) => onMouseEnter(e)}
+            onMouseLeave={(e) => onMouseLeave(e)}
+          >
+            <AiOutlineArrowLeft color={hoverState.left ? 'white' : 'black'} />
+          </button>
+          <button
+            name="right"
+            className={`flex justify-center items-center absolute top-[45%] right-[10%] ${
+              hoverState.right
+                ? 'bg-main scale-125 ease-in duration-120 transition-all'
+                : 'bg-white scale-100 ease-in duration-120 transition-all border-white'
+            } w-[40px] h-[40px] border-[1px] border-transparent rounded-3xl`}
+            onClick={scrollNext}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <AiOutlineArrowRight color={hoverState.right ? 'white' : 'black'} />
+          </button>
+        </>
+      )}
+      <div className="absolute left-[50%] -translate-x-1/2 bottom-1 flex gap-[5px]">
+        {imgs.map((_, idx) => (
+          <button
+            key={`${_}-${idx}`}
+            className={`p-0 w-[14px] h-[14px] rounded-full border ${
+              index === idx && 'bg-[rgba(255,255,255,0.9)] w-[24px]'
+            }`}
+            onClick={() => setIndex(idx)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+ */
--- components/introduce/IntroduceDesc.tsx
+++ components/introduce/IntroduceDesc.tsx
@@ -6,10 +6,14 @@ import { isMobileDisplay } from '../../store/isMobileDisplay'
 const descs = [
   {
     img: '/profileImage.png',
-    title: '안녕하세요 푸디풀 입니다.',
+    title: '안녕하세요.',
     desc: '다양한 경험을 바탕으로 재밌고 즐거운 교육을 진행하는 푸디풀 대표 "전민지"입니다.',
   },
-  { img: '/foodiful.jpeg', title: '푸디풀 입니다', desc: '엄청난 이력의 소유자 입니다.' },
+  {
+    img: '/foodiful.jpeg',
+    title: '푸디풀 입니다.',
+    desc: 'Food와 Beautiful을 더한 단어로 우리나라의 음식을 우리나라 모든 사람들과  세계의 다양한 사람들에게 알리기 위해 노력합니다.',
+  },
 ]
 const IntroduceDesc = () => {
   const isMobile = useAtomValue(isMobileDisplay)
@@ -19,18 +23,18 @@ const IntroduceDesc = () => {
         ({ title, desc, img }: { title: string; desc: string; img: string }, idx: number) => (
           <div
             key={`${title}-${desc}`}
-            className={`flex ${isMobile ? 'flex-col items-center' : 'justify-center'} `}
+            className={`flex ${isMobile ? 'flex-col items-center' : 'justify-center gap-x-4'} `}
           >
             <div
               className={`${
-                !isMobile ? ((idx + 1) % 2 === 0 ? 'order-last w-[40%]' : 'w-[30%]') : 'break-keep'
+                !isMobile ? ((idx + 1) % 2 === 0 ? 'order-last w-[30%]' : 'w-[30%]') : 'break-keep'
               }`}
               key={`${title}-${img}-${idx}`}
             >
               <StrongTitle title={title} style="my-[20px]" />
-              <div className="font-semibold text-xl">{desc}</div>
+              <div className="font-semibold text-xl leading-10">{desc}</div>
             </div>
-            <div className={`mt-5 ${isMobile ? 'flex justify-center' : 'relative w-[40%]'}`}>
+            <div className={`mt-5 ${isMobile ? 'flex justify-center' : 'relative w-[30%]'}`}>
               <Image
                 src={img}
                 alt="main-image"
