c40f68a760908226588f8a0d388b5fea123e4091
davidktlee <rudxor567@naver.com>
2024-06-21 14:26:02 +0900

    fix: 무한 스크롤 로직 수정

--- components/review/ReviewList.tsx
+++ components/review/ReviewList.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import useIntersectionObserver from '../common/hooks/useIntersectionObserver'
 import ReviewItem from './ReviewItem'
 import { ProductReviewTypes } from './types/productReviewTypes'
@@ -9,41 +9,32 @@ interface PropsType {
 
 const ReviewList = ({ reviewList }: PropsType) => {
   const [renderReviewStartCount, setRenderReviewStartCount] = useState(0)
-  const [renderReviewEndCount, setRenderReviewEndCount] = useState(10)
+  const [renderReviewEndCount, setRenderReviewEndCount] = useState(5)
+  const [renderReviewList, setRenderReviewList] = useState<ProductReviewTypes[]>([])
 
   const ref = useRef<HTMLDivElement>(null)
 
   const addRenderReviewCount = () => {
-    if (reviewList.length <= renderReviewStartCount) {
-      return
-    }
-    setRenderReviewStartCount((prev) => prev + 10)
-    setRenderReviewEndCount((prev) => prev + 10)
-    const updatedList = reviewList.slice(renderReviewStartCount, renderReviewEndCount)
+    setRenderReviewStartCount((prev) => prev + 5)
+    setRenderReviewEndCount((prev) => prev + 5)
+    const updatedList = reviewList.slice(renderReviewStartCount + 5, renderReviewEndCount + 5)
     setRenderReviewList([...renderReviewList, ...updatedList])
   }
   useIntersectionObserver(ref, addRenderReviewCount)
 
-  const [renderReviewList, setRenderReviewList] = useState<ProductReviewTypes[]>([])
-
+  useEffect(() => {
+    setRenderReviewStartCount(0)
+    setRenderReviewEndCount(5)
+    setRenderReviewList(reviewList.slice(0, 5))
+  }, [reviewList])
   return (
     <>
-      {!!reviewList.length ? (
-        <>
-          <div className="flex-col justify-center">
-            {renderReviewList.map((review) => (
-              <ReviewItem key={`${review.id}`} review={review} />
-            ))}
-          </div>
-          <div ref={ref}></div>
-        </>
-      ) : (
-        <div className="w-full flex justify-center items-center mt-[40px]">
-          <span className="text-textDisabled text-2xl font-bold">
-            리뷰가 존재하지 않습니다. <br /> 상품을 구매하시고 리뷰를 등록 해주세요!
-          </span>
-        </div>
-      )}
+      <div className="flex-col justify-center">
+        {renderReviewList.map((review) => (
+          <ReviewItem key={`${review.id}`} review={review} />
+        ))}
+      </div>
+      {reviewList.length > renderReviewEndCount && <div ref={ref}>ref임</div>}
     </>
   )
 }
