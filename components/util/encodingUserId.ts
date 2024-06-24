359dbf0d5f3b1b24fd89d7173fe513a0bb567659
davidktlee <rudxor567@naver.com>
2024-06-24 13:18:19 +0900

    chore: 유틸 함수 삭제

--- components/util/encodingUserId.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../auth/types/user'
-
-export const encodingUserId = (user: User) => {
-  return user.role === 'ADMIN'
-    ? user.id
-    : user.id.toString().length > 5
-    ? `${user.id.toString().slice(0, 4)}****`
-    : `${user.id}****`
-}
