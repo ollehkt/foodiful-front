0adfc80b17b4a13ceef73b9ecb10529a89937cd7
davidktlee <rudxor567@naver.com>
2024-06-24 13:31:00 +0900

    chore: 후기 페이지 글귀 수정 및 보여지는 로직 수정

--- pages/mypage/reviews/index.tsx
+++ pages/mypage/reviews/index.tsx
@@ -5,6 +5,7 @@ import { useGetReviewByUserId } from '../../../components/review/hooks/useReview
 import { User } from '../../../components/auth/types/user'
 import { getStoredUser } from '../../../components/util/userStorage'
 import ReviewList from '../../../components/review/ReviewList'
+import Link from 'next/link'
 
 const MyPageMyComments = () => {
   const [user, _] = useState<User | null>(typeof window !== 'undefined' ? getStoredUser() : null)
@@ -13,7 +14,16 @@ const MyPageMyComments = () => {
   return (
     <section className="grow flex-col items-center px-5 mt-10">
       <StrongTitle title="내 후기 보기" style="border-b-2 border-main pb-2" />
-      <ReviewList reviewList={myReviews} />
+      {!myReviews.length ? (
+        <div className="w-full flex flex-col items-center text-2xl font-bold my-10">
+          <div>작성한 후기가 없습니다.</div>
+          <Link className="text-main hover:text-hover mt-10" href="/product">
+            상품 주문하러 가기
+          </Link>
+        </div>
+      ) : (
+        <ReviewList reviewList={myReviews} />
+      )}
     </section>
   )
 }
