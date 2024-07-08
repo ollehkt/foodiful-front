b1b4cf77dc7f506092ce3ec97277542054d08dee
davidktlee <rudxor567@naver.com>
2024-07-08 22:01:28 +0900

    fix: nextjs 버전 업으로 middleware 에러 수정

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const refreshToken = request.cookies.get('refresh')?.value
   if (!refreshToken) return NextResponse.redirect(new URL('/auth', request.url))
   else return NextResponse.next()
