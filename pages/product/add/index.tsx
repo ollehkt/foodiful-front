ddee68017c2efeb5ab66b19101990b90f2ee974b
davidktlee <rudxor567@naver.com>
2024-07-09 13:13:35 +0900

    feat: 상품 추가 페이지 타이틀 추가

--- pages/product/add/index.tsx
+++ pages/product/add/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useAddProduct } from '../../../components/product/hooks/useProduct'
 import ProductForm from '../../../components/product/ProductForm'
+import StrongTitle from '../../../components/common/StrongTitle'
 
 function ProductAddPage() {
   const { addProductMutate } = useAddProduct()
 
   return (
     <>
+      <StrongTitle title="제품 추가" style="border-b-2 border-main pb-2 my-16 mx-4" />
       <ProductForm onSubmitAdd={addProductMutate} />
     </>
   )
