6477ff44c17e7a89fc1642286aa2340e7a9c68ee
davidktlee <rudxor567@naver.com>
2024-07-09 13:49:57 +0900

    feat: 클래스 추가 페이지 타이틀 추가

--- pages/lecture/add/index.tsx
+++ pages/lecture/add/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import LectureForm from '../../../components/lecture/LectureForm'
 import { useAddLecture } from '../../../components/lecture/hooks/useLecture'
+import StrongTitle from '../../../components/common/StrongTitle'
 
 function LectureAddPage() {
   const { addLectureMutate } = useAddLecture()
-  return <LectureForm onSubmitAdd={addLectureMutate} />
+  return (
+    <>
+      <StrongTitle title="클래스 추가" style="border-b-2 border-main pb-2 my-16 mx-4" />
+      <LectureForm onSubmitAdd={addLectureMutate} />
+    </>
+  )
 }
 
 export default LectureAddPage
