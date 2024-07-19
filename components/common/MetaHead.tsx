20cfe9a1082a23725cf4727949a644f04fbdfd6a
davidktlee <rudxor567@naver.com>
2024-07-19 23:50:40 +0900

    fix: og 이미지 경로 수정

--- components/common/MetaHead.tsx
+++ components/common/MetaHead.tsx
@@ -18,14 +18,14 @@ const MetaHead = ({ title, description, url, img }: PropsType) => {
       <meta property="og:title" content={title || '푸디풀 FOODIFUL'} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url || 'https://www.foodiful.shop'} />
-      <meta property="og:image" content={img || 'https://foodiful.shop/public/foodiful.jpeg'} />
+      <meta property="og:image" content={img || 'https://www.foodiful.shop/public/foodiful.jpeg'} />
       <meta property="og:article:author" content="푸디풀 FOODIFUL" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="foodiful.shop" />
       <meta property="twitter:url" content="https://www.foodiful.shop" />
       <meta name="twitter:title" content="푸디풀 FOODIFUL" />
       <meta name="twitter:description" content="한식 디저트의 모든 것, 푸디풀" />
-      <meta name="twitter:image" content="https://foodiful.shop/public/foodiful.jpeg" />
+      <meta name="twitter:image" content="https://www.foodiful.shop/public/foodiful.jpeg" />
     </Head>
   )
 }
